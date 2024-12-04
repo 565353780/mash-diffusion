5ea14ea0ca493eca8e29305dd4100c297d3c0a9b
chLi <565353780@qq.com>
2024-12-04 23:50:47 +0800

    add sample images to video method

---
+++ sample_to_video.py
@@ -0,0 +1,19 @@
+from conditional_flow_matching.Method.video import sampleImagesFolderToVideos
+
+
+if __name__ == "__main__":
+    iter_root_folder_path = './output/render_sample/20241204_18:06:56/'
+    save_video_folder_path = './output/video_sample/20241204_18:06:56/'
+    video_width = 540
+    video_height = 540
+    video_fps = 24
+    overwrite = False
+
+    sampleImagesFolderToVideos(
+        iter_root_folder_path,
+        save_video_folder_path,
+        video_width,
+        video_height,
+        video_fps,
+        overwrite
+    )
