ce259d71d5a0f023ee122894da508338457218e1
chLi <565353780@qq.com>
2024-12-11 14:53:38 +0800

    create and test batch OT-CFM passed

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from conditional_flow_matching.Test.fid import test as test_fid
+from conditional_flow_matching.Test.batch_ot_cfm import test as test_batch_ot_cfm
 
 if __name__ == "__main__":
-    test_fid()
+    # test_fid()
+    test_batch_ot_cfm()
