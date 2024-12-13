35960a1f2f0791417535719b4b6635d49ffd6227
chLi <565353780@qq.com>
2024-12-13 20:07:43 +0800

    create a complete demo script for test

--- conditional_flow_matching/Test/fm.py
+++ conditional_flow_matching/Test/fm.py
@@ -1,9 +1,116 @@
+import time
 import torch
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from torch import nn, Tensor
 
+from flow_matching.path.scheduler import CondOTScheduler
+from flow_matching.path import AffineProbPath
 from flow_matching.solver import ODESolver
 from flow_matching.utils import ModelWrapper
+
+if torch.cuda.is_available():
+    device = 'cuda:0'
+    print('Using gpu')
+else:
+    device = 'cpu'
+    print('Using cpu.')
+
+torch.manual_seed(42)
+
+def inf_train_gen(batch_size: int = 200, device: str = "cpu"):
+    x1 = torch.rand(batch_size, device=device) * 4 - 2
+    x2_ = torch.rand(batch_size, device=device) - torch.randint(high=2, size=(batch_size, ), device=device) * 2
+    x2 = x2_ + (torch.floor(x1) % 2)
+
+    data = 1.0 * torch.cat([x1[:, None], x2[:, None]], dim=1) / 0.45
+    
+    return data.float()
+
+# Activation class
+class Swish(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x: Tensor) -> Tensor: 
+        return torch.sigmoid(x) * x
+
+# Model class
+class MLP(nn.Module):
+    def __init__(self, input_dim: int = 2, time_dim: int = 1, hidden_dim: int = 128):
+        super().__init__()
+        
+        self.input_dim = input_dim
+        self.time_dim = time_dim
+        self.hidden_dim = hidden_dim
+
+        self.main = nn.Sequential(
+            nn.Linear(input_dim+time_dim, hidden_dim),
+            Swish(),
+            nn.Linear(hidden_dim, hidden_dim),
+            Swish(),
+            nn.Linear(hidden_dim, hidden_dim),
+            Swish(),
+            nn.Linear(hidden_dim, hidden_dim),
+            Swish(),
+            nn.Linear(hidden_dim, input_dim),
+            )
+    
+
+    def forward(self, x: Tensor, t: Tensor) -> Tensor:
+        sz = x.size()
+        x = x.reshape(-1, self.input_dim)
+        t = t.reshape(-1, self.time_dim).float()
+
+        t = t.reshape(-1, 1).expand(x.shape[0], 1)
+        h = torch.cat([x, t], dim=1)
+        output = self.main(h)
+        
+        return output.reshape(*sz)
+
+lr = 0.001
+batch_size = 4096
+iterations = 20001
+print_every = 2000 
+hidden_dim = 512
+
+# velocity field model init
+vf = MLP(input_dim=2, time_dim=1, hidden_dim=hidden_dim).to(device) 
+
+# instantiate an affine path object
+path = AffineProbPath(scheduler=CondOTScheduler())
+
+# init optimizer
+optim = torch.optim.Adam(vf.parameters(), lr=lr) 
+
+# train
+start_time = time.time()
+for i in range(iterations):
+    optim.zero_grad() 
+
+    # sample data (user's responsibility): in this case, (X_0,X_1) ~ pi(X_0,X_1) = N(X_0|0,I)q(X_1)
+    x_1 = inf_train_gen(batch_size=batch_size, device=device) # sample data
+    x_0 = torch.randn_like(x_1).to(device)
+
+    # sample time (user's responsibility)
+    t = torch.rand(x_1.shape[0]).to(device) 
+
+    # sample probability path
+    path_sample = path.sample(t=t, x_0=x_0, x_1=x_1)
+
+    # flow matching l2 loss
+    loss = torch.pow( vf(path_sample.x_t,path_sample.t) - path_sample.dx_t, 2).mean() 
+
+    # optimizer step
+    loss.backward() # backward
+    optim.step() # update
+    
+    # log loss
+    if (i+1) % print_every == 0:
+        elapsed = time.time() - start_time
+        print('| iter {:6d} | {:5.2f} ms/step | loss {:8.3f} ' 
+              .format(i+1, elapsed*1000/print_every, loss.item())) 
+        start_time = time.time()
 
 class WrappedModel(ModelWrapper):
     def forward(self, x: torch.Tensor, t: torch.Tensor, **extras):
@@ -17,6 +124,7 @@ step_size = 0.05
 norm = cm.colors.Normalize(vmax=50, vmin=0)
 
 batch_size = 50000  # batch size
+eps_time = 1e-2
 T = torch.linspace(0,1,10)  # sample times
 T = T.to(device=device)
 
@@ -31,17 +139,17 @@ fig, axs = plt.subplots(1, 10,figsize=(20,20))
 
 for i in range(10):
     H= axs[i].hist2d(sol[i,:,0], sol[i,:,1], 300, range=((-5,5), (-5,5)))
-    
+
     cmin = 0.0
     cmax = torch.quantile(torch.from_numpy(H[0]), 0.99).item()
-    
+
     norm = cm.colors.Normalize(vmax=cmax, vmin=cmin)
-    
+
     _ = axs[i].hist2d(sol[i,:,0], sol[i,:,1], 300, range=((-5,5), (-5,5)), norm=norm)
-    
+
     axs[i].set_aspect('equal')
     axs[i].axis('off')
     axs[i].set_title('t= %.2f' % (T[i]))
-    
+
 plt.tight_layout()
 plt.show()
