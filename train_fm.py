bbcfe322fceb4c342ff12724b225b558ee41afdb
chLi <565353780@qq.com>
2024-12-14 19:13:24 +0800

    remove archive same trainer

--- train_fm.py
+++
@@ -1,4 +0,0 @@
-from conditional_flow_matching.Demo.fm_trainer import demo as demo_train_flow_matching
-
-if __name__ == "__main__":
-    demo_train_flow_matching()
--- train_fm.sh
+++
@@ -1,1 +0,0 @@
-OMP_NUM_THREADS=16 torchrun --nproc_per_node=1 train_fm.py
