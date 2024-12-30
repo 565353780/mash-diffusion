3443937ac16840c5283e79b3c82c5878b5b2bb57
chLi <565353780@qq.com>
2024-12-30 14:37:21 +0000

    add custom gpu ids contorl

--- train.sh
+++ train.sh
@@ -1,1 +1,6 @@
-OMP_NUM_THREADS=16 torchrun --nproc_per_node=1 --master_port 29500 train.py
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 \
+  OMP_NUM_THREADS=16 \
+  torchrun \
+  --nproc_per_node=7 \
+  --master_port 29500 \
+  train.py
