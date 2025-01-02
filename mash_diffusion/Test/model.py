fca69a0d077402d7ab42fbb74431e052a4dada3c
chLi <565353780@qq.com>
2025-01-02 11:55:39 +0800

    create model test for check cross attention usage

---
+++ mash_diffusion/Test/model.py
@@ -0,0 +1,26 @@
+import torch
+from mash_diffusion.Model.cfm_latent_transformer import CFMLatentTransformer
+
+def test():
+    device = 'cpu'
+
+    model = CFMLatentTransformer(
+        n_latents=400,
+        mask_degree=3,
+        sh_degree=2,
+        context_dim=1024,
+        n_heads=16,
+        d_head=64,
+        depth=24
+    ).to(device)
+
+    data_dict = {
+        'mash_params': torch.randn([2, 400, 25], device=device),
+        'condition': torch.randn([2, 1397, 1024], device=device),
+        't': torch.randn([2], device=device),
+        'drop_prob': 0.0,
+    }
+
+    result_dict = model(data_dict)
+
+    return True
--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 from mash_diffusion.Test.fid import test as test_fid
 from mash_diffusion.Test.batch_ot_cfm import test as test_batch_ot_cfm
 from mash_diffusion.Test.fm import test as test_flow_matching
+from mash_diffusion.Test.model import test as test_model
 
 if __name__ == "__main__":
     # test_fid()
     # test_batch_ot_cfm()
-    test_flow_matching()
+    # test_flow_matching()
+    test_model()
