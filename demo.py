6d82c64fd2b565488e2ffd51e3dde7b90148a7d1
chLi <565353780@qq.com>
2025-01-04 14:01:55 +0800

    remove unused code, add more demo scripts call

--- demo.py
+++ demo.py
@@ -1,8 +1,10 @@
-from mash_diffusion.Demo.cfm_trainer import demo as demo_train_cfm
 from mash_diffusion.Demo.edm_trainer import demo as demo_train_edm
-from mash_diffusion.Demo.sampler import demo as demo_sample
+from mash_diffusion.Demo.cfm_trainer import demo as demo_train_cfm
+from mash_diffusion.Demo.edm_sampler import demo as demo_sample_edm
+from mash_diffusion.Demo.cfm_sampler import demo as demo_sample_cfm
 
 if __name__ == "__main__":
-    demo_train_cfm()
     demo_train_edm()
-    demo_sample()
+    demo_train_cfm()
+    demo_sample_edm()
+    demo_sample_cfm()
