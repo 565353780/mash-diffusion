6dc6baf50079414788abed2299b924ee4a9e3492
chLi <565353780@qq.com>
2025-07-15 16:57:01 +0800

    add cfm hy3ddit model size check script

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from mash_diffusion.Test.fid import test as test_fid
 # from mash_diffusion.Test.batch_ot_cfm import test as test_batch_ot_cfm
 from mash_diffusion.Test.model import test as test_model
 from mash_diffusion.Test.hy3ddit import test as test_hy3ddit
+from mash_diffusion.Test.cfm_hy3ddit import test as test_cfm_hy3ddit
 from mash_diffusion.Test.tos import test as test_tos
 
 if __name__ == "__main__":
@@ -10,4 +11,5 @@ if __name__ == "__main__":
     # test_batch_ot_cfm()
     # test_model()
     # test_hy3ddit()
-    test_tos()
+    test_cfm_hy3ddit()
+    # test_tos()
